7fd25887a82a7a17b84de7c2ebd972e775848ded
kim <145192439+KimNattanan@users.noreply.github.com>
2023-10-30 21:03:35 +0700

    Create floyd_warshall.cpp

---
+++ graph/floyd_warshall.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define pb push_back
+#define pii pair<int,int>
+#define f first
+#define s second
+#define ll long long
+
+int dp[305][305];
+
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int n,m; cin>>n>>m;
+    memset(dp,0x3f,sizeof dp);
+    for(int i=0;i<m;++i){
+        int u,v,w; cin>>u>>v>>w;
+        dp[u][v]=dp[v][u]=min(dp[u][v],w);
+    }
+    for(int k=1;k<=n;++k){
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=n;++j){
+                dp[i][j]=min(dp[i][j],dp[i][k]+dp[k][j]);
+            }
+        }
+    }
+
+    return 0;
+}
+/*
+
+
+*/
