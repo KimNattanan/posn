bc2a11008d5e801e217382b509356d7358c85f7d
kim <145192439+KimNattanan@users.noreply.github.com>
2023-10-30 20:20:47 +0700

    Create dijkstra.cpp

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define pb push_back
+#define pii pair<int,int>
+#define f first
+#define s second
+
+vector<pii> adj[100005];
+struct A{
+    int u,w;
+    bool operator<(const A&o)const{
+        return w>o.w;
+    }
+};
+int dist[100005];
+
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int n,m; cin>>n>>m;
+    for(int i=0;i<m;++i){
+        int u,v,w; cin>>u>>v>>w;
+        adj[u].pb({v,w});
+        adj[v].pb({u,w});
+    }
+    int S,T; cin>>S>>T;
+    priority_queue<A> pq;
+    pq.push({S,0});
+    memset(dist,0x3f,sizeof dist);
+    dist[S]=0;
+    while(!pq.empty()){
+        int u=pq.top().u;
+        int w=pq.top().w;
+        pq.pop();
+        for(auto &vw:adj[u]){
+            if(dist[vw.f]>w+vw.s){
+                dist[vw.f]=w+vw.s;
+                pq.push({vw.f,w+vw.s});
+            }
+        }
+    }
+    cout<<dist[T]<<endl;
+
+    return 0;
+}
+/*
+
+*/
