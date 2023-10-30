4e5c14de539e3256a4699d8184b1061044a7fab4
kim <145192439+KimNattanan@users.noreply.github.com>
2023-10-30 21:27:17 +0700

    Create union_by_rank.cpp

---
+++ graph/DSU_on_tree/union_by_rank.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define pb push_back
+#define pii pair<int,int>
+#define f first
+#define s second
+#define ll long long
+
+int P[100005],rnk[100005];
+
+int fSet(int u){
+    if(P[u]==u) return u;
+    return fSet(P[u]);
+}
+
+bool uSet(int u,int v){
+    int U=fSet(u),V=fSet(v);
+    if(U==V) return 0;
+    if(rnk[U]>rnk[V]) swap(U,V);
+    P[U]=V;
+    if(rnk[U]==rnk[V]) ++rnk[V];
+    return 1;
+}
