4bffc27435a96cf8b2242fff2e2e983e8022fd29
kim <145192439+KimNattanan@users.noreply.github.com>
2023-11-28 10:36:13 +0700

    Update euler_tour.cpp

--- graph/euler_tour.cpp
+++ graph/euler_tour.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 #define endl '\n'
 #define pb push_back
-#define pii pair<int,int>
+using ll=long long;
+using pii=pair<int,int>;
 #define f first
 #define s second
-#define ll long long
 
 vector<int> adj[100005];
 int tour[100005],ed[100005],id0;
