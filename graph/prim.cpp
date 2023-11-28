b42f6e2bcfb88ec3c545bef94b9b197d15232ae2
kim <145192439+KimNattanan@users.noreply.github.com>
2023-11-28 10:36:46 +0700

    Update prim.cpp

--- graph/prim.cpp
+++ graph/prim.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 #define endl '\n'
 #define pb push_back
-#define pii pair<int,int>
+using ll=long long;
+using pii=pair<int,int>;
 #define f first
 #define s second
-#define ll long long
 
 ll dist[5005];
 ll f(pii a,pii b){
