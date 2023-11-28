87e524f7628d4ea6142e5921b2ebb3309a2c00e6
kim <145192439+KimNattanan@users.noreply.github.com>
2023-11-28 14:38:11 +0700

    Update knapsack1.cpp

--- dp/Knapsack/knapsack1.cpp
+++ dp/Knapsack/knapsack1.cpp
@@ -18,7 +18,7 @@ int main(){
     
     for(int i=1;i<=n;++i){
         for(int j=1;j<=k;++j){
-            dp[i][j]=dp[i-1][j];
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             if(j>=w[i]) dp[i][j]=max(dp[i][j],dp[i-1][j-w[i]]+val[i]);
         }
     }
