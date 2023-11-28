9c018e9630b0e3af4f73e2f6391c3709ecf46e6e
kim <145192439+KimNattanan@users.noreply.github.com>
2023-11-28 15:01:51 +0700

    Update knapsack0.cpp

--- dp/Knapsack/knapsack0.cpp
+++ dp/Knapsack/knapsack0.cpp
@@ -8,7 +8,7 @@ using pii=pair<int,int>;
 #define pb push_back
 
 int w[2005],val[2005];
-int dp[2005][10005];
+int dp[2][10005];
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(0);
@@ -16,12 +16,17 @@ int main(){
     int n,k; cin>>n>>k;
     for(int i=1;i<=n;++i) cin>>w[i]>>val[i];
     
+    dp[0][0]=0;
+    for(int j=1;j<=k;++j) dp[0][j]=-1e9;
+    
     for(int i=1;i<=n;++i){
+        int now=i%2,prev=1-now;
         for(int j=1;j<=k;++j){
-            dp[i][j]=dp[i-1][j];
-            if(j>=w[i]) dp[i][j]=max(dp[i][j],dp[i-1][j-w[i]]+val[i]);
+            dp[now][j]=dp[prev][j];
+            if(j>=w[i]) dp[now][j]=max(dp[now][j],dp[prev][j-w[i]]+val[i]);
         }
     }
+    cout<<dp[n%2][k];
     
     return 0;
 }
