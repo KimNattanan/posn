24e1cd49236c4efd0f5e4501c9c8f9a6019f27a0
kim <145192439+KimNattanan@users.noreply.github.com>
2023-11-28 15:10:56 +0700

    Update knapsack0_opt1.cpp

---
+++ dp/Knapsack/knapsack0_opt1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+using ll=long long;
+using pii=pair<int,int>;
+#define f first
+#define s second
+#define pb push_back
+
+int w[2005],val[2005];
+int dp[10005];
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    
+    int n,k; cin>>n>>k;
+    for(int i=1;i<=n;++i) cin>>w[i]>>val[i];
+    
+    for(int i=1;i<=n;++i){
+        for(int j=k;j>=w[i];--j){
+            dp[j]=max(dp[j],dp[j-w[i]]+val[i]);
+        }
+    }
+    
+    return 0;
+}
