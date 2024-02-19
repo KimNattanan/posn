ee3370d73c10936414ededb144ad6a3c9d760823
kim <145192439+KimNattanan@users.noreply.github.com>
2024-02-19 16:30:30 +0700

    Update fenwick.cpp

--- tree/fenwick/fenwick.cpp
+++ tree/fenwick/fenwick.cpp
@@ -1,21 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-using ll=long long;
 
 struct fenwick{
     vector<int> bit;
-    int n;
-    void init(int n_){
-        n=n_;
-        bit=vector<int>(n);
-    }
-    void add(int i,int x){
-        for(;i<n;i+=i&-i) bit[i]+=x;
+    fenwick(int n=0){init(n);}
+    void init(int n){bit.assign(n,0);}
+    void upd(int i,int x){
+        for(;i<bit.size();i+=i&-i) bit[i]+=x;
     }
     int qr(int i){
-        int res=0;
-        for(;i>0;i-=i&-i) res+=bit[i];
-        return res;
+        int ret=0;
+        for(;i>0;i-=i&-i) ret+=bit[i];
+        return ret;
+    }
+    void upd(int l,int r,int x){ upd(l,x), upd(r+1,-x); }
+    int qr(int l,int r){ return qr(r)-qr(l-1); }
+    int search(int x){
+        int sum=0,pos=0;
+        for(int i=1<<__lg(bit.size());i>0;i>>=1){
+            if(sum+bit[pos+i]<=x) pos+=i, sum+=bit[pos];
+        }
+        return pos;
     }
 };
