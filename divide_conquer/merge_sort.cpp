1f45db15f5567a942e70412012e6114338d76c3d
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-29 09:49:54 +0700

    Update merge_sort.cpp

--- divide_conquer/merge_sort.cpp
+++ divide_conquer/merge_sort.cpp
@@ -1,43 +1,17 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-using ll=long long;
-using pii=pair<int,int>;
-#define f first
-#define s second
-#define pb push_back
 
-void merge_sort(vector<int> &vec){
-    int n=vec.size();
-    if(n==1) return;
-    vector<int> L,R;
-    for(int i=0;i<n/2;++i) L.pb(vec[i]);
-    for(int i=n/2;i<n;++i) R.pb(vec[i]);
-    merge_sort(L),merge_sort(R);
-    vec.clear();
-    int l=0,r=0;
-    while(l<L.size()&&r<R.size()){
-        if(L[l]<R[r]) vec.pb(L[l++]);
-        else vec.pb(R[r++]);
-    }
-    while(l<L.size()) vec.pb(L[l++]);
-    while(r<R.size()) vec.pb(R[r++]);
+int a[200005],a_[200005];
+
+ll merge_sort(int l,int r){
+  if(l==r) return 0;
+  int mid=l+r>>1;
+  merge_sort(l,mid), merge_sort(mid+1,r);
+  for(int il=l, ir=mid+1, i=l; i<=r;){
+    if(il>mid) a_[i++]=a[ir++];
+    else if(ir>r) a_[i++]=a[il++];
+    else if(a[il]<a[ir]) a_[i++]=a[il++];
+    else a_[i++]=a[ir++];
+  }
+  for(int i=l;i<=r;++i) a[i]=a_[i];
 }
-
-int32_t main(){
-    ios::sync_with_stdio(false); cin.tie(0);
-
-    int n; cin>>n;
-    vector<int> vec(n);
-    for(int i=0;i<n;++i){
-        cin>>vec[i];
-    }
-    merge_sort(vec);
-    for(auto &e:vec) cout<<e<<" ";
-    cout<<endl;
-
-    return 0;
-}
-/**
-
-*/
