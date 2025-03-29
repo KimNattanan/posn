3f3dfe1e4b583b984557acc63bf6da84df0e679a
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-29 09:48:50 +0700

    Update inversion_count.cpp

--- divide_conquer/inversion_count.cpp
+++ divide_conquer/inversion_count.cpp
@@ -1,47 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
 using ll=long long;
-using pii=pair<int,int>;
-#define f first
-#define s second
-#define pb push_back
 
-int inv_cnt(vector<int> &vec){
-    int n=vec.size();
-    if(n==1) return 0;
-    vector<int> L,R;
-    for(int i=0;i<n/2;++i) L.pb(vec[i]);
-    for(int i=n/2;i<n;++i) R.pb(vec[i]);
-    int ans=inv_cnt(L)+inv_cnt(R);
-    vec.clear();
-    int l=0,r=0;
-    while(l<L.size()&&r<R.size()){
-        if(L[l]>R[r]){
-            ans+=(L.size()-l);
-            vec.pb(R[r++]);
-        }
-        else{
-            vec.pb(L[l++]);
-        }
-    }
-    while(l<L.size()) vec.pb(L[l++]);
-    while(r<R.size()) vec.pb(R[r++]);
-    return ans;
+int a[200005],a_[200005];
+
+ll inv_count(int l,int r){
+  if(l==r) return 0;
+  int mid=l+r>>1;
+  ll res=inv_count(l,mid)+inv_count(mid+1,r);
+  for(int il=l, ir=mid+1, i=l; i<=r;){
+    if(il>mid) a_[i++]=a[ir++];
+    else if(ir>r) a_[i++]=a[il++];
+    else if(a[il]>a[ir]) res += mid-il+1, a_[i++]=a[ir++];
+    else a_[i++]=a[il++];
+  }
+  for(int i=l;i<=r;++i) a[i]=a_[i];
+  return res;
 }
-
-int32_t main(){
-    ios::sync_with_stdio(false); cin.tie(0);
-
-    int n; cin>>n;
-    vector<int> vec(n);
-    for(int i=0;i<n;++i){
-        cin>>vec[i];
-    }
-    cout<<inv_cnt(vec)<<endl;
-
-    return 0;
-}
-/**
-
-*/
