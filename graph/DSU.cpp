8dd1ee29c16e741af331812b9f36f68af39e7c1b
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-31 20:47:53 +0700

    Update DSU.cpp

--- graph/DSU.cpp
+++ graph/DSU.cpp
@@ -1,20 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define pb push_back
-using ll=long long;
-using pii=pair<int,int>;
-#define f first
-#define s second
 
-int P[100005];
-int fSet(int u){
-    if(P[u]==u) return u;
-    return P[u]=fSet(P[u]);
-}
+int p[100005],sz[100005];
+
+int fSet(int u){ return p[u]==u ? u : p[u]=fSet(p[u]); }
+
 bool uSet(int u,int v){
-    int U=fSet(u),V=fSet(v);
-    if(U==V) return 0;
-    P[U]=V;
-    return 1;
+  int U=fSet(u),V=fSet(v);
+  if(U==V) return 0;
+  p[U]=V;
+  return 1;
 }
