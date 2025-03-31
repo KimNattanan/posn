58c8249e8ff89a9cd98a4d3acd4622f3f0a95b9c
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-31 17:41:06 +0700

    Update Lavenshtein.cpp

--- dp/Lavenshtein.cpp
+++ dp/Lavenshtein.cpp
@@ -1,27 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define sz(x) (int)x.size()
 
 int dp[5005][5005];
 
 int main(){
-    ios::sync_with_stdio(false); cin.tie(0);
+  ios::sync_with_stdio(false); cin.tie(0);
 
-    string a,b; cin>>a>>b;
-    int lena=a.size(),lenb=b.size();
-    a=" "+a;
-    b=" "+b;
-    for(int i=1;i<=lena;++i) dp[i][0]=i;
-    for(int j=1;j<=lenb;++j) dp[0][j]=j;
-    for(int i=1;i<=lena;++i){
-        for(int j=1;j<=lenb;++j){
-            if(a[i]==b[j]) dp[i][j]=dp[i-1][j-1];
-            else dp[i][j]=min({dp[i-1][j],  // delete
-                               dp[i][j-1],  // insert
-                               dp[i-1][j-1] // change
-                             })+1;
-	    }
-	}
-	cout<<dp[lena][lenb];
-
-	return 0;
+  string a,b; cin>>a>>b;
+  for(int i=1;i<=sz(a);++i) dp[i][0]=i;
+  for(int j=1;j<=sz(b);++j) dp[0][j]=j;
+  for(int i=1;i<=sz(a);++i){
+    for(int j=1;j<=sz(b);++j){
+      if(a[i-1]==b[j-1]) dp[i][j]=dp[i-1][j-1];
+      else dp[i][j]=min({ dp[i-1][j],  // delete
+                          dp[i][j-1],  // insert
+                          dp[i-1][j-1] // change
+                        })+1;
+    }
+  }
+	cout<<dp[sz(a)][sz(b)];
 }
