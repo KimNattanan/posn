bd26730b3426fb6048d648ccd266b81c0ea36b34
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-31 20:33:21 +0700

    Update dynamic_segment.cpp

--- data_structure/segment_tree/dynamic_segment.cpp
+++ data_structure/segment_tree/dynamic_segment.cpp
@@ -30,4 +30,11 @@ struct Segment{
     if(l<=il&&ir<=r) return t;
     return calc(ch[0].qr(l,r),ch[1].qr(l,r));
   }
+  int ub(ll x){
+    flush();
+    if(t[0]<=x) return ir+1;
+    if(il==ir) return il;
+    if(ch[0].t[0]>x) return ch[0].ub(x);
+    return ch[1].ub(x);
+  }
 }t;
