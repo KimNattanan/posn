a9a343fa90180c7fb5ad783efb14f7f76e40f720
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-31 21:06:10 +0700

    Update dfs_bfs.cpp

--- graph/dfs_bfs.cpp
+++ graph/dfs_bfs.cpp
@@ -1,48 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define pb push_back
 
 vector<int> adj[100005];
-bool flag[100005];
+bitset<100005> vis;
+int d[100005];
+queue<int> q;
 
 void dfs(int u){
-    flag[u]=1;
-    for(auto &v:adj[u]){
-        if(!flag[v]) dfs(v);
-    }
+  vis[u]=1;
+  for(auto &v:adj[u]) if(!vis[v]){
+    d[v]=d[u]+1;
+    dfs(v);
+  }
 }
 
-void bfs(int U){
-    queue<int> q;
-    q.push(U);
-    flag[U]=1;
-    while(!q.empty()){
-        int u=q.front();
-        q.pop();
-        for(auto &v:adj[u]){
-            if(!flag[v]){
-                flag[v]=1;
-                q.push(v);
-            }
-        }
+void bfs(int u){
+  vis[u]=1;
+  d[u]=0;
+  q.emplace(u);
+  while(!q.empty()){
+    int u=q.front(); q.pop();
+    for(auto &v:adj[u]) if(!vis[v]){
+      vis[v]=1;
+      d[v]=d[u]+1;
+      q.emplace(v);
     }
+  }
 }
-
-int32_t main(){
-    ios::sync_with_stdio(false); cin.tie(0);
-
-    int n,m; cin>>n>>m;
-    for(int i=0;i<m;++i){
-        int u,v; cin>>u>>v;
-        adj[u].pb(v);
-        adj[v].pb(u);
-    }
-    dfs(1);
-    bfs(1);
-
-    return 0;
-}
-/*
-
-*/
