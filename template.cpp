fd84da39ab51edae94f373630f2c533b491422ff
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-31 18:30:32 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+#define f first
+#define s second
+#define eb emplace_back
+#define sz(x) (int)x.size()
+#define add(x,y) ((((x)+(y))%md+md)%md)
+#define Add(x,y) (x=add(x,y))
+#define mul(x,y) ((((x)*(y))%md+md)%md)
+#define Mul(x,y) (x=mul(x,y))
+template<class T> T chmn(T &x,T y){ return x=min(x,y); }
+template<class T> T chmx(T &x,T y){ return x=max(x,y); }
+const int inf=1e9;
+const ll linf=1e18;
+const ll md=1e9+7;
+// const ll md=119<<23|1;
+
+void solve(){
+
+}
+
+int main(){
+  ios::sync_with_stdio(false); cin.tie(0);
+
+  int Q(1);
+  cin>>Q;
+  while(Q--) solve();  
+
+}
+/*
+
+
+*/
