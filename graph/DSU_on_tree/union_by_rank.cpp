8c1513e2b18c46aadbb349c0f033a5bf1d70b15f
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-31 20:46:47 +0700

    Update union_by_rank.cpp

--- graph/DSU_on_tree/union_by_rank.cpp
+++ graph/DSU_on_tree/union_by_rank.cpp
@@ -1,24 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define pb push_back
-using ll=long long;
-using pii=pair<int,int>;
-#define f first
-#define s second
 
-int P[100005],rnk[100005];
+int p[100005],rnk[100005];
 
-int fSet(int u){
-    if(P[u]==u) return u;
-    return fSet(P[u]);
-}
+int fSet(int u){ return p[u]==u ? u : p[u]=fSet(p[u]); }
 
 bool uSet(int u,int v){
-    int U=fSet(u),V=fSet(v);
-    if(U==V) return 0;
-    if(rnk[U]>rnk[V]) swap(U,V);
-    P[U]=V;
-    if(rnk[U]==rnk[V]) ++rnk[V];
-    return 1;
+  int U=fSet(u),V=fSet(v);
+  if(U==V) return 0;
+  if(rnk[U]>rnk[V]) swap(U,V);
+  p[U]=V;
+  if(rnk[U]==rnk[V]) ++rnk[V];
+  return 1;
 }
