38d3e224a1d35a5384a38b7e00fe71b3c539fe7c
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-31 17:52:21 +0700

    Update kadane.cpp

--- dp/kadane.cpp
+++ dp/kadane.cpp
@@ -1,18 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
+using ll=long long;
 
-int32_t main(){
-    ios::sync_with_stdio(false); cin.tie(0);
+ll a[1000005],dp[1000005];
 
-    int n; cin>>n;
-    int now=0,mx=-1e9;
-    for(int i=1;i<=n;++i){
-        int x; cin>>x;
-        now=max(now+x,x);
-        mx=max(mx,now);
-    }
-    cout<<mx<<endl;
+int main(){
+  ios::sync_with_stdio(false); cin.tie(0);
 
-    return 0;
+  int n; cin>>n;
+  for(int i=1;i<=n;++i) cin>>a[i];
+  for(int i=1;i<=n;++i) dp[i] = max(a[i], dp[i-1]+a[i]);
 }
