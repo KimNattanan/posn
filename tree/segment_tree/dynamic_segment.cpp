d3d15944306666e0196439c3f0a67d6cdd2ac8bc
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-31 19:29:56 +0700

    Create dynamic_segment.cpp

---
+++ tree/segment_tree/dynamic_segment.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define sz(x) (int)x.size()
+#define eb emplace_back
+
+struct Segment{
+  using A=array<ll,3>;
+  A t;
+  vector<Segment> ch;
+  int il,ir;
+  Segment(int l,int r):il(l),ir(r),t(A{0,0,0}){}
+  A calc(A l,A r){
+    return { l[0]+r[0], min(l[1],r[1]), max(l[2],r[2]) };
+  }
+  void flush(){
+    if(il!=ir && ch.empty()) ch.eb(il,il+ir>>1), ch.eb((il+ir>>1)+1,ir);
+  }
+  void upd(int id,ll x){
+    flush();
+    if(il==ir) return void(t[0]=t[1]=t[2]+=x);
+    int mid=il+ir>>1;
+    if(id<=mid) ch[0].upd(id,x);
+    else ch[1].upd(id,x);
+    t=calc(ch[0].t, ch[1].t);
+  }
+  A qr(int l,int r){
+    flush();
+    if(il>r||ir<l) return { 0, 1e18, -1e18 };
+    if(l<=il&&ir<=r) return t;
+    return calc(ch[0].qr(l,r),ch[1].qr(l,r));
+  }
+}t;
