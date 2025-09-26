4983062a8c87c9f463afd09ede4ab4a44d752500
kim <145192439+KimNattanan@users.noreply.github.com>
2025-09-26 15:31:14 +0700

    Update o(n)_cc.cpp

--- o(n)_cc.cpp
+++ o(n)_cc.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int cc[1000005],nc;
 
-void f(vector<int> &v){
+void compress(vector<int> &v){
   unordered_map<int,int> mp;
-  for(auto &e:A) if(!mp[e]) cc[++nc]=e, e=mp[e]=nc;
-  for(auto &e:B) if(!mp[e]) cc[++nc]=e, e=mp[e]=nc;
+  for(auto &e:v) if(!mp[e]) cc[++nc]=e, e=mp[e]=nc;
 }
