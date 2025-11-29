04a49fefcf41d5cef70b0b943a34ee5b95c83dbb
kim <145192439+KimNattanan@users.noreply.github.com>
2025-11-29 16:00:27 +0700

    Update bellman_ford.cpp

--- graph/bellman_ford.cpp
+++ graph/bellman_ford.cpp
@@ -1,55 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define pb push_back
-using ll=long long;
-using pii=pair<int,int>;
-#define f first
-#define s second
 
-struct E{
-    int u,v;
-    ll w;
-};
-vector<E> edge;
-ll dist[5005],dist0[5005];
+const int inf=1e9;
 
-int32_t main(){
-    ios::sync_with_stdio(false); cin.tie(0);
+using i3=array<int,3>;
+vector<i3> edge;
+int d[200005];
 
-    int n,m; cin>>n>>m;
-    memset(dist,0x3f,sizeof dist);
-    edge.resize(m);
-    for(int i=0;i<m;++i) cin>>edge[i].u>>edge[i].v>>edge[i].w;
-    int S,T; cin>>S>>T;
-    dist[S]=0;
-    for(int i=0;i<n-1;++i){
-        for(auto &e:edge){
-            if(dist[e.v]>dist[e.u]+e.w){
-                dist[e.v]=dist[e.u]+e.w;
-            }
-        }
-    }
-    for(int i=1;i<=n;++i) dist0[i]=dist[i];
-    bool ok=1;
-    for(auto &e:edge){
-        if(dist[e.v]>dist[e.u]+e.w){
-            dist[e.v]=dist[e.u]+e.w;
-            ok=0;
-        }
-    }
-    if(!ok){
-        cout<<"negative cycle."<<endl;
-        for(int i=1;i<=n;++i){
-            if(dist[i]!=dist0[i]) cout<<i<<" ";
-        }
-        cout<<endl;
-    }
-    else cout<<dist[T]<<endl;
+int main(){
+  ios::sync_with_stdio(false); cin.tie(0);
 
-    return 0;
+  int n,m; cin>>n>>m;
+  edge.resize(m);
+  for(auto &[u,v,w]:edge) cin>>u>>v>>w;
+  fill(d,d+n+1,inf);
+  
+  int s; cin>>s;
+  d[s]=0;
+  for(int i=0;i<n-1;++i) for(auto &[u,v,w]:edge){
+    if(d[u]!=inf) d[v] = min(d[v], d[u]+w);
+  }
+  bool neg=false;
+  for(auto &[u,v,w]:edge) if(d[u]!=inf && d[u]+w < d[v]){
+    neg=1;
+    break;
+  }
+
 }
-/*
-
-
-*/
